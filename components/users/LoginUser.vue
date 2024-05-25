6de4cb99d6bdc47954ca2a66c249a780e419cafe
SinNombre 89 <je.rodriguezvazquez@ugto.mx>
2024-05-24 21:38:32 -0600

    Empezamos a trabajar front

---
+++ components/users/LoginUser.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-card>
+    <v-card-title>
+      Inicio de Sesión
+    </v-card-title>
+    <v-card-text>
+      Van los input
+    </v-card-text>
+    <v-card-actions>
+      Botones
+    </v-card-actions>
+  </v-card>
+</template>
+
+<script>
+export default {
+
+}
+</script>
---
+++ layouts/login.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-app>
+    <v-main>
+      <v-row
+        style="width: 100%; height: 100vh;"
+        justify="center"
+        align="center"
+      >
+        <Nuxt />
+      </v-row>
+    </v-main>
+  </v-app>
+</template>
