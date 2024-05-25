1dbdc1be06ca9d6ffca8aded31b8d36c9413edae
SinNombre 89 <je.rodriguezvazquez@ugto.mx>
2024-05-25 00:42:10 -0600

    Inicio de un Login (necesita Pruebas)

--- components/users/LoginUser.vue
+++ components/users/LoginUser.vue
@@ -1,13 +1,50 @@
 <template>
-  <v-card>
+  <v-card
+    color="#84FFFF"
+    elevation="0"
+    width="500"
+  >
     <v-card-title>
       Inicio de Sesión
     </v-card-title>
     <v-card-text>
-      Van los input
+      <v-form
+        ref="form"
+        v-model="validForm"
+      >
+        Email:
+        <v-text-field
+          v-model="email"
+          placeholder="Escribe tu correo"
+          type="email"
+          :rules="correo"
+        />
+        Password:
+        <v-text-field
+          v-model="passwordUser"
+          placeholder="Escribe tu password"
+          type="password"
+          :rules="password"
+        />
+      </v-form>
     </v-card-text>
     <v-card-actions>
-      Botones
+      <v-row>
+        <v-col cols="6">
+          <v-btn block color="#212121" elevation="0" @click="loginUser">
+            <span style="text-transform: none; color: white">
+              Login
+            </span>
+          </v-btn>
+        </v-col>
+        <v-col cols="6">
+          <v-btn block color="#BDBDBD" elevation="0" @click="showNuevo = true">
+            <span style="text-transform: none; color: white">
+              Signup
+            </span>
+          </v-btn>
+        </v-col>
+      </v-row>
     </v-card-actions>
   </v-card>
 </template>
